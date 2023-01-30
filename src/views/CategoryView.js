d9840a151629fa164bed2044b2275faac4044a26
laurentbedu <bedulaurent@gmail.com>
2023-01-30 15:53:25 +0100

    E01

--- src/views/CategoryView.js
+++ src/views/CategoryView.js
@@ -1,10 +1,14 @@
 export class CategoryView {
-    
-    constructor(categoryName){
-        this.categoryName = categoryName;
-    }
-    
-    render = () => {
-      return `Categorie ${this.categoryName}`;
-    }
+  
+  constructor(categoryName) {
+    this.categoryName = categoryName;
+  }
+
+  render = () => {
+    return `
+      <div id="CategoryDetailView">
+          <h1>La catégorie ${this.categoryName}</h1>
+      </div>
+      `;
+  };
 }
