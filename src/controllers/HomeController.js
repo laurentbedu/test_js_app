ffcf3d60a690d5534815cc8227289098886471e5
laurentbedu <bedulaurent@gmail.com>
2023-01-31 08:42:37 +0100

    E03

--- src/controllers/HomeController.js
+++ src/controllers/HomeController.js
@@ -1,8 +1,12 @@
 export class HomeController{
 
     async execute(){
+        //Models
+        const resp = await fetch("../src/data/category.json");
+        const categories = await resp.json();
+        //view
         const {HomeView} = await import('../views/HomeView.js');
-        const view = new HomeView();
+        const view = new HomeView(categories);
         const content = view.render();
         return content;
         
--- src/views/HomeView.js
+++ src/views/HomeView.js
@@ -1,8 +1,37 @@
 export class HomeView {
 
+    constructor(categories){
+        this.categories = categories;
+    }
+
     //TODO categories
 
   render = () => {
+
+    const categoriesCards = this.categories.map(category => {
+        return `
+        <div class="col-12 col-md-6 col-lg-4 pb-3">
+            <div class="card" onclick="location.href='/category/${category.title.replace(' ','-')}'">
+                <img src="${category.image}" class="card-img-top" alt="${category.title}">
+                <div class="card-body">
+                    <h5 class="card-title">${category.title}</h5>
+                    <p class="card-text">${category.description}</p>
+                </div>
+            </div>
+        </div>
+        `;
+    }).join('');
+
+    return `
+    <link rel="stylesheet" href="/assets/css/HomeView.css">
+    <div id="HomeView">
+        <h1>Page d'Accueil - Liste des catégories</h1>
+        <div class="row">
+            ${categoriesCards}
+        </div>
+    </div>
+    `;
+
     return `
     <link rel="stylesheet" href="/assets/css/HomeView.css">
     <div id="HomeView">
